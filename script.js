ba7b111a734f67ea3d943c5d9aa7085d56a53e5c
Adrian <adunham95@gmail.cooom>
2017-11-11 22:58:13 -0500

    Ability to add people

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let tower = [];
 let money;
+let citizen = [];
 
 //Creates a new tower
 function newTower() {
     window.localStorage.setItem("TowerID", generateID());
     window.localStorage.setItem("Money", 5000);
-    window.localStorage.setItem("TowerFloors", JSON.stringify(tower))
+    window.localStorage.setItem("TowerFloors", JSON.stringify(tower));
+    // citizen = [];
+    window.localStorage.setItem("Tenates", JSON.stringify(citizen));
+
 }
 
 function onLoad() {
@@ -34,6 +38,9 @@ function onLoad() {
     //Displays the amount of the money the user has
     money = window.localStorage.getItem("Money");
     document.getElementById("money").innerText = '$'+money;
+
+    //Gets the tenants
+    citizen = JSON.parse(window.localStorage.getItem("Tenates"));
 
     for(let i = 0; i < tower.length; i++){
        displayFloor(i)
@@ -70,11 +77,12 @@ function Apartment(name, floor, colors) {
     }
 }
 
-function Person(name, home, job) {
+function Person(name, bio, home, job) {
     //Creates a person in the tower
 
     this.id = generateID();
     this.name = name;
+    this.bio = bio;
     this.home = home;
     this.job = job;
 }
@@ -83,34 +91,45 @@ function generateID() {
     //Generates a random 6 digit id
     let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26)) + String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
     String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Math.floor(Math.random() * 9) + Math.floor(Math.random() * 9) +Math.floor(Math.random() * 9);
-    console.log(randLetter);
     return randLetter;
 }
 
-function createNewFloor() {
+function createNewFloor(randomness) {
     let newFloor;
     let name = document.getElementById("name").value;
     let type = document.getElementById("type").value;
-    let category = document.getElementById("category").value;
+    let category;
     let color = [document.getElementById("color").value];
     let floor = (tower.length);
+
+    //Random Floor
+    if(randomness === 'random'){
+        category = getRandom('category');
+        color = [getRandom("color")];
+    }
+    //Custom Floor
+    else {
+        category = document.getElementById("category").value;
+    }
+
+    //If no color is selected
+    if(color === '#fff'){
+        color = [getRandom("color")];
+    }
 
     //If the name is blank it will be the category value
     if(name === ''){
         name = category;
     }
 
-    //If no color is selected
-    if(color === '#fff'){
-        color = [randColor()];
-    }
-
+    //If Store or apartment
     if (type === "Store"){
         newFloor = new Store(name, category, floor, color);
     }
     else {
         newFloor = new Apartment(name, floor, color);
     }
+
     console.log(newFloor);
 
     tower.push(newFloor);
@@ -124,13 +143,53 @@ function createNewFloor() {
     document.getElementById("money").innerText = '$'+money;
 }
 
-function randColor() {
-    let colors = ['#f7556e', '#b3187d', '#d44d47', '#7fafea', '#aa78db', '#8b6715', '#3a8f96', '#7b35b0', '#98d70c', '#d0793e'];
-    return colors[Math.floor(Math.random() * colors.length)]
+function createNewPerson() {
+    let newPerson;
+    let name = [];
+    let bio = document.getElementById("bio").value;
+
+    // If the name is blank it will be the category value
+    if(document.getElementById("firstName").value==='' && document.getElementById("lastName").value===''){
+        console.log("Getting random");
+        name = [getRandom("first"),getRandom("last")];
+    }
+    else {
+        console.log("Given");
+        console.log(document.getElementById("firstName").value);
+        name = [document.getElementById("firstName").value, document.getElementById("lastName").value];
+    }
+
+    newPerson = new Person(name, bio, 1, 1);
+    // console.log(newPerson);
+
+    citizen.push(newPerson);
+    console.log(citizen);
+    window.localStorage.setItem("Tenates", JSON.stringify(citizen));
+
+}
+
+
+function getRandom(type) {
+    if(type === "color"){
+        let colors = ['#f7556e', '#b3187d', '#d44d47', '#7fafea', '#aa78db', '#8b6715', '#3a8f96', '#7b35b0', '#98d70c', '#d0793e'];
+        return colors[Math.floor(Math.random() * colors.length)]
+    }
+    else if (type === "first"){
+        let first = ["John", "Steve"];
+        return first[Math.floor(Math.random() * (first.length))];
+    }
+    else if (type === "last"){
+        let last = ["Smith", "Hyde", "Stevens"];
+        return last[Math.floor(Math.random() * last.length)];
+    }
+    else if (type === 'category'){
+        let categories = ["Car Dealership", "Grocery Store", "Jewellery Store", "Flower Store", "Beauty Salon", "Butcher", "Toy Store", "Music Store", "Clothes Store","Book Store","Tech Store","Sports Store","E-Store","Dollar Store","Brand Store"];
+        return categories[Math.floor(Math.random() * categories.length)]
+    }
 }
 
 function displayFloor(i) {
-    console.log(tower[i]);
+    // console.log(tower[i]);
 
     //Creates the elements
     let targetElement = document.getElementById("tower");
